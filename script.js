b1902e0db959dbe0e8e9a6c78855e0c965d3b606
GTX1050TiGato <165506305+GTX1050TiGato@users.noreply.github.com>
2024-05-28 20:33:01 -0300

    Atualização de contatos e botões

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 function baixar() {
     const conteudoDiv = document.getElementById('conteudo');
     const qrCodeImage = document.createElement('img');
+    
     qrCodeImage.src = 'qr-code-plus.png'; // Caminho para a sua imagem QR code
     qrCodeImage.alt = 'QR Code';
-    qrCodeImage.classList.add('h-52', 'bg-white', 'rounded-md');
+    qrCodeImage.classList.add('w-1/3', 'bg-white', 'rounded-md', 'clickme');
 
     // Adicionar a transição ao estilo
     qrCodeImage.style.transition = 'opacity 1s';
@@ -18,6 +19,7 @@ function baixar() {
     }
 
     conteudoDiv.appendChild(qrCodeImage);
+    
 
     // Usar setTimeout para aplicar o efeito fade-in após adicionar o elemento ao DOM
     setTimeout(() => {
@@ -59,24 +61,34 @@ function numero() {
 function contato() {
     const conteudoDiv = document.getElementById('conteudo');
     const h1 = document.createElement('h1');
-    h1.classList.add('text-5xl', 'text-white', 'text-nowrap', 'zap');
+    const h1map = document.createElement('h1');
+    const mapElement = document.createElement('img'); // Changed variable name to mapElement
+    h1.classList.add('text-5xl', 'text-white', 'text-nowrap', 'zap', 'scale-50');
     h1.textContent = '(91) 3351-3245'; // Definir o texto do <h1>
 
-            // Adicionar a transição ao estilo
-            h1.style.transition = 'opacity 1s';
+    h1map.classList.add('text-3xl', 'text-white','scale-75');
+    h1map.textContent = 'Endereço no google maps:'; // Definir o texto do <h1>
 
-            // Inicialmente, definir a opacidade como 0 para o efeito fade-in
-            h1.style.opacity = 0;
+    mapElement.src = 'Mapimg.png'; // Changed from map.scr to map.src
+    mapElement.classList.add('bg-white', 'rounded-md', 'clickme', 'mt-5');
+
+    // Adicionar a transição ao estilo
+    h1.style.transition = 'opacity 1s';
+
+    // Inicialmente, definir a opacidade como 0 para o efeito fade-in
+    h1.style.opacity = 0;
+    
     // Limpar qualquer conteúdo existente na div
     while (conteudoDiv.firstChild) {
         conteudoDiv.removeChild(conteudoDiv.firstChild);
     }
 
-    // Adicionar o <h1> à div
+    // Adicionar o <h1> e o mapa à div
     conteudoDiv.appendChild(h1);
-
+    conteudoDiv.appendChild(h1map);
+    conteudoDiv.appendChild(mapElement);
+    
     setTimeout(() => {
         h1.style.opacity = 1;
     }, 10); // Pequeno atraso para garantir que o navegador registre a mudança de opacidade
-
 }
